43ce207dab16159e8a96d937f6a301b6eebf3d60
Oni-Men <sensyaheis@gmail.com>
2022-08-27 15:38:53 +0900

    remove unnecessary code

--- cmd/runner.go
+++ cmd/runner.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"github.com/Oni-Men/SchedulePoll/bot"
 )
 
 func goMain() int {
-	flag.Parse()
-
 	s := bot.Create(os.Getenv("DISCORD_BOT_TOKEN"))
 	if s == nil {
 		return 1
